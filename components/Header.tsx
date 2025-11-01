df4d9b064b0de40569e181073296e281b2ce615c
kianush00 <k.atighimoghadda01@ufromail.cl>
2025-11-01 00:55:08 -0300

    added new components

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,8 +5,14 @@ import Container from "./Container";
 import MobileMenu from "./MobileMenu";
 import SearchBar from "./SearchBar";
 import CartIcon from "./CartIcon";
+import { currentUser } from "@clerk/nextjs/server";
+import { ClerkLoaded, SignedIn, SignInButton, UserButton } from "@clerk/nextjs";
+import { ListOrdered } from "lucide-react";
+import Link from "next/link";
 
-const Header = () => {
+const Header = async () => {
+  const user = await currentUser();
+  console.log("user", user);
   return (
     <header className="border-b border-b-gray-200 py-5">
       <Container className="flex items-center justify-between gap-7 text-lightColor">
@@ -18,11 +24,24 @@ const Header = () => {
         <div className="w-auto md:w-1/3 flex items-center justify-end gap-5">
           <SearchBar />
           <CartIcon />
-          <div>
-            <button className="text-sm font-semibold hover:text-darkColor hoverEffect">
-              Login
-            </button>
-          </div>
+          <ClerkLoaded>
+            <SignedIn>
+              <Link href={"/orders"} className="group relative">
+                <ListOrdered className="w-5 h-5 group-hover:text-darkColor hoverEffect" />
+                <span className="absolute -top-1 -right-1 bg-darkColor text-white h-3.5 w-3.5 rounded-full text-xs font-semibold flex items-center justify-center">
+                  0
+                </span>
+              </Link>
+              <UserButton />
+            </SignedIn>
+            {!user && (
+              <SignInButton mode="modal">
+                <button className="text-sm font-semibold hover:text-darkColor hoverEffect">
+                  Login
+                </button>
+              </SignInButton>
+            )}
+          </ClerkLoaded>
         </div>
       </Container>
     </header>
---
+++ components/Sidebar.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { motion } from "motion/react";
+import Logo from "./Logo";
+import { X } from "lucide-react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { headerData } from "@/app/constants";
+import SocialMedia from "./SocialMedia";
+import { useOutsideClick } from "@/hooks/useOutsideClick";
+
+interface Props {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const Sidebar = ({ isOpen, onClose }: Props) => {
+  const pathname = usePathname();
+  const sidebarRef = useOutsideClick<HTMLDivElement>(onClose);
+  return (
+    <div
+      className={`fixed inset-y-0 left-0 z-50 bg-darkColor/50 shadow-xl hoverEffect cursor-auto w-full ${
+        isOpen ? "translate-x-0" : "-translate-x-full"
+      }`}
+    >
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.4, delay: 0.3 }}
+        ref={sidebarRef}
+        className="min-w-72 max-w-96 bg-darkColor text-white/70 h-full p-10 border-r border-r-white flex flex-col gap-6"
+      >
+        <div className="flex items-center justify-between">
+          <button onClick={onClose}>
+            <Logo className="text-white">Joyas Sol</Logo>
+          </button>
+          <button className="hover:text-red-500 hoverEffect" onClick={onClose}>
+            <X />
+          </button>
+        </div>
+        <div className="flex flex-col gap-3.5 text-base font-semibold tracking-wide">
+          {headerData?.map((item) => (
+            <Link
+              key={item?.title}
+              href={item?.href}
+              className={`hover:text-white hoverEffect w-24 ${
+                item?.href === pathname && "text-white"
+              }`}
+            >
+              {item?.title}
+            </Link>
+          ))}
+        </div>
+        <SocialMedia />
+      </motion.div>
+    </div>
+  );
+};
+
+export default Sidebar;
